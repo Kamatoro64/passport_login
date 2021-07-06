bcc48a302290441a54bd3076f9c6ce46dfbaf5c2
Chester Ng <chester91.jp@gmail.com>
2021-07-06 15:35:41 +0000

    Setting up login.ejs and register.ejs and their corresponding routes, adding body parser middleware to express so that we can access variables passed from the form

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
 
+// Re-implement this using postgreSQL later
+const users = []
+
 app.set('view-engine', 'ejs')
+
+// Allow us to access variables sent from the forms
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'kamatoro' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+
 })
 app.listen(3000)
