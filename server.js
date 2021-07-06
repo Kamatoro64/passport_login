34f083c950be4217225d3b4082c648383cea4757
Chester Ng <chester91.jp@gmail.com>
2021-07-06 15:59:07 +0000

    Implement POST register route: store user object in users array with password encrypted using bcrypt. users array to be replaced with postgres

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 // Re-implement this using postgreSQL later
 const users = []
@@ -25,7 +26,23 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
-
+app.post('/register', async (req, res) => {
+    try{
+        console.log(req.body)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push(
+            {
+                id: Date.now().toString(), // replace with identity column in db
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            }
+        )
+        res.redirect('/login')
+    } catch
+    {
+        res.redirect('/register')
+    }
+    console.log(users)
 })
 app.listen(3000)
